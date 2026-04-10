b55c9b5af1b9b6d5361564c2df552101d9e964b2
SuniL <sunilgumatimath.vs@gmail.com>
2026-04-10 15:19:16 +0530

    feat: add FastAPI app with Inngest RAG ingest PDF function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import logging
+import uuid
 
-from dotenv.main import logger
-from fastapi import FastAPI
 import inngest
 import inngest.fast_api
-from inngest.experimental import ai
 from dotenv import load_dotenv
-import uuid 
-import os
-import datetime
+from fastapi import FastAPI
+
+from custom_types import (
+    RAGChunkAndSrc,
+    RAGUpsertResult,
+)
+from data_loader import embed_texts, load_and_chunk_pdf
+from vector_db import QdrantStorage
 
 load_dotenv()
 
@@ -16,20 +19,45 @@ inngest_client = inngest.Inngest(
     app_id="rag_app",
     logger=logging.getLogger("uvicorn"),
     is_production=False,
-    serializer=inngest.PydanticSerializer()
+    serializer=inngest.PydanticSerializer(),
 )
+
 
 @inngest_client.create_function(
-    fn_id="RAG: Ingest PDF",
-    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+    fn_id="RAG: Ingest PDF", trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
 )
+async def rag_ingest_pdf(ctx: inngest.Context):
+    async def _load(ctx: inngest.Context) -> RAGChunkAndSrc:
+        pdf_path = ctx.event.data["pdf_path"]
+        source_id = ctx.event.data.get("source_id", pdf_path)
+        chunks = load_and_chunk_pdf(str(pdf_path))
+        return RAGChunkAndSrc(chunks=chunks, source_id=str(source_id))
 
-async def rag_ingest_pdf(ctx:inngest.Context):
-    return {"hello" : "world"}
+    async def _upsert(chunks_and_src: RAGChunkAndSrc) -> RAGUpsertResult:
+        chunks = chunks_and_src.chunks
+        source_id = chunks_and_src.source_id
+        vecs = embed_texts(chunks)
+        ids = [
+            str(uuid.uuid5(uuid.NAMESPACE_URL, f"{source_id}:{i}"))
+            for i in range(len(chunks))
+        ]
+        payloads = [
+            {"source": source_id, "text": chunks[i]} for i in range(len(chunks))
+        ]
+        QdrantStorage().upsert(ids, vecs, payloads)
+        return RAGUpsertResult(ingested=len(chunks))
+
+    chunks_and_src = await ctx.step.run(
+        "load_and-chunk", lambda: _load(ctx), output_type=RAGChunkAndSrc
+    )
+    ingested = await ctx.step.run(
+        "embed-and-upsert",
+        lambda: _upsert(chunks_and_src),
+        output_type=RAGUpsertResult,
+    )
+    return ingested.model_dump()
 
 
 app = FastAPI()
 
-
-
-inngest.fast_api.serve(app,inngest_client,[])
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
